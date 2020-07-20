e0a0e7c75ec3e3db6b25e1f6aacacca07a9fc34b
aschoen <aschoen@clever-zoeger.de>
2020-07-21 00:13:58 +0200

    simplified nav submenu

--- src/App/Page.js
+++ src/App/Page.js
@@ -1,19 +1,5 @@
 export class Page {
-  static closeSubmenuHandler(event) {
-    if (!event.target.closest('.has-submenu')) {
-      event.preventDefault();
-      Page.closeSubmenu();
-    }
-  }
-
-  static closeSubmenu(event) {
-    document.querySelector('.has-submenu').classList.remove('open');
-    document.body.removeEventListener('click', Page.closeSubmenuHandler);
-  }
-
   constructor() {
-    this.hasSubmenu = document.querySelector('.has-submenu');
-    this.hasSubmenuLink = document.getElementById('has-submenu-link');
     this.adminLoginOpenBtn = document.querySelector('.admin-login-btn');
     this.adminLoginCloseBtn = document.querySelector('.admin-login-close');
   }
@@ -23,17 +9,9 @@ export class Page {
   }
 
   setPageEvents() {
-    this.hasSubmenuLink.addEventListener('click', this.openSubmenu.bind(this));
     this.adminLoginForm = document.querySelector('.admin-login-form');
     this.adminLoginOpenBtn.addEventListener('click', this.openAdminLogin.bind(this));
     this.adminLoginCloseBtn.addEventListener('click', this.closeAdminLogin.bind(this));
-  }
-
-  openSubmenu(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    this.hasSubmenu.classList.add('open');
-    document.body.addEventListener('click', Page.closeSubmenuHandler);
   }
 
   openAdminLogin() {
